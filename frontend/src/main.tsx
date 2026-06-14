cc11b67573846eaf9bcf08ac4d3188eaa810bc25
Sudhanshu singh <sudhanshu6073@gmail.com>
2026-06-15 00:38:10 +0530

    main styling and app file improve

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AppRouter } from "./app/AppRouter";
+import { ToastHost } from "./components/ui/ToastHost";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppRouter />
+    <ToastHost />
   </React.StrictMode>,
 );
